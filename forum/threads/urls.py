fec7b06e75a0cf941331023ba6d8ea5e459a4768
elliot <postsneakernet@gmail.com>
2015-05-16 00:24:41 -0400

    Change topic detail url name

--- forum/threads/urls.py
+++ forum/threads/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.Home.as_view(), name='home'),
     url(r'^topics/$', views.TopicIndex.as_view(), name='topics'),
-    url(r'^topics/(?P<topic_slug>\S+)$', views.TopicDetail.as_view(), name = 'thread_index'),
+    url(r'^topics/(?P<topic_slug>\S+)$', views.TopicDetail.as_view(), name = 'topic_detail'),
 
     # url(r'^(?P<thread_slug>\S+)$', views.Thread.as_view(), name='Thread')
 ]
