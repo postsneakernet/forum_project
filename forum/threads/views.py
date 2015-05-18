47721966f8e64146e8e90dc9da2c3cc27c738ecc
elliot <postsneakernet@gmail.com>
2015-05-17 23:11:51 -0400

    Add thread creation functionality

--- forum/threads/views.py
+++ forum/threads/views.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+from django.contrib import messages
+from django.db.models import Count
 from django.views import generic
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils.text import slugify
 from .models import Thread, Reply, Topic
-from django.db.models import Count
+
 import operator
 
 
@@ -27,10 +30,11 @@ class Home(generic.ListView):
     template_name = 'home.html'
     context_object_name = 'threads'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, error="", **kwargs):
         context = super(Home, self).get_context_data(**kwargs)
         context['side_threads'] = side_bar_threads()
         context['topics'] = Topic.objects.all()
+        context['error'] = error
         return context
 
     def get_queryset(self):
@@ -53,6 +57,7 @@ class TopicIndex(generic.ListView):
 class TopicDetail(generic.ListView):
     template_name = 'topic_detail.html'
     context_object_name = 'threads'
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super(TopicDetail, self).get_context_data(**kwargs)
@@ -76,15 +81,51 @@ def thread_detail(request, topic_slug, thread_slug):
         author = request.POST.get('author')
         if author:
             new_reply.author = author
+
         new_reply.thread = thread
-        new_reply.body = request.POST.get('body', '')
-        new_reply.save()
+        body = request.POST.get('body')
+        if body:
+            new_reply.body = body
+            new_reply.save()
+            messages.success(request, "New comment successfully added!")
+        else:
+            messages.error(request, "Oops, there was an error adding comment.")
         return redirect('thread_detail', topic_slug, thread_slug)
 
     return render(request, 'thread_detail.html', {
             'thread': thread, 'replies': replies, 'topics': topics,
             'side_threads': side_threads,
-            })
+    })
+
 
 def create(request):
-    return render(request, 'create.html')
+    topics = Topic.objects.all()
+    side_threads = side_bar_threads()
+
+    if request.POST:
+        new_thread = Thread()
+        author = request.POST.get('author')
+        title = request.POST.get('title')
+        topic_id = request.POST.get('topic_id')
+        body = request.POST.get('body')
+        if author and title and topic_id and body:
+            slug = slugify(title)
+
+            if Thread.objects.filter(slug=slug).exists():
+                return redirect('create')
+
+            new_thread.author = author
+            new_thread.title = title
+            new_thread.slug = slug
+            new_thread.topic = Topic.objects.get(id=topic_id)
+            new_thread.save()
+            messages.success(request,"New thread successfully created!")
+            return redirect('/')
+        else:
+            messages.error(request, "Oops, there was an error creating thread.")
+            return redirect('create')
+
+
+    return render(request, 'create.html', {
+            'topics': topics, 'side_threads': side_threads,
+    })
