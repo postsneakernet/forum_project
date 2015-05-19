25356e020c1426ea79dcd92deac979fc7e85b7ea
elliot <postsneakernet@gmail.com>
2015-05-18 22:34:22 -0400

    Add side threads and topics to context

--- forum/forum/views.py
+++ forum/forum/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from threads.views import side_bar_threads
+from threads.models import Topic
+
 
 def login(request):
-    return render(request, 'login.html')
+    side_threads = side_bar_threads()
+    topics = Topic.objects.all()
+
+    return render(request, 'login.html', {
+            'side_threads': side_threads, 'topics': topics,
+    })
